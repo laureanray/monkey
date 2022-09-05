f62f77ac928e1cbe65f91ebd578aab4d1017e93a
laureanray <laureanraybahala@gmail.com>
2022-09-06 00:19:33 +0800

    add IntegerLiteral

--- ast/ast.go
+++ ast/ast.go
@@ -20,9 +20,14 @@ type Expression interface {
 	expressionNode()
 }
 
-type Program struct {
-	Statements []Statement
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
 }
+
+func (il *IntegerLiteral) expessionNode()       {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
 
 type LetStatement struct {
 	Token token.Token
@@ -90,6 +95,10 @@ type Identifier struct {
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 func (i *Identifier) String() string       { return i.Value }
+
+type Program struct {
+	Statements []Statement
+}
 
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
