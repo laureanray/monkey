9b6c1f23d95c4668118c48d7365f4d51366e3ecd
laureanray <laureanraybahala@gmail.com>
2022-09-18 21:46:08 +0800

    add parser tracing

--- parser/parser.go
+++ parser/parser.go
@@ -250,7 +250,11 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 	precedence := p.curPrecedence()
 	p.nextToken()
 
-	expression.Right = p.parseExpression(precedence)
+	if expression.Operator == "+" {
+		expression.Right = p.parseExpression(precedence - 1)
+	} else {
+		expression.Right = p.parseExpression(precedence)
+	}
 
 	return expression
 }
