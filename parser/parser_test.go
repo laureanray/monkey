204356432dd4d891502b3d31ca17519f3a7431cf
laureanray <laureanraybahala@gmail.com>
2022-09-23 15:15:19 +0800

    uncomment test for return stmt

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -41,42 +41,42 @@ func TestLetStatements(t *testing.T) {
 	}
 }
 
-//func TestReturnStatements(t *testing.T) {
-//	tests := []struct {
-//		input         string
-//		expectedValue interface{}
-//	}{
-//		{"return 5;", 5},
-//		{"return true;", true},
-//		{"return foobar;", "foobar"},
-//	}
-//
-//	for _, tt := range tests {
-//		l := lexer.New(tt.input)
-//		p := New(l)
-//		program := p.ParseProgram()
-//		checkParserErrors(t, p)
-//
-//		if len(program.Statements) != 1 {
-//			t.Fatalf("program.Statements does not contain 1 statements. got=%d",
-//				len(program.Statements))
-//		}
-//
-//		stmt := program.Statements[0]
-//		returnStmt, ok := stmt.(*ast.ReturnStatement)
-//		if !ok {
-//			t.Fatalf("stmt not *ast.ReturnStatement. got=%T", stmt)
-//		}
-//		if returnStmt.TokenLiteral() != "return" {
-//			t.Fatalf("returnStmt.TokenLiteral not 'return', got %q",
-//				returnStmt.TokenLiteral())
-//		}
-//
-//		if testLiteralExpression(t, returnStmt.ReturnValue, tt.expectedValue) {
-//			return
-//		}
-//	}
-//}
+func TestReturnStatements(t *testing.T) {
+	tests := []struct {
+		input         string
+		expectedValue interface{}
+	}{
+		{"return 5;", 5},
+		{"return true;", true},
+		{"return foobar;", "foobar"},
+	}
+
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors(t, p)
+
+		if len(program.Statements) != 1 {
+			t.Fatalf("program.Statements does not contain 1 statements. got=%d",
+				len(program.Statements))
+		}
+
+		stmt := program.Statements[0]
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Fatalf("stmt not *ast.ReturnStatement. got=%T", stmt)
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Fatalf("returnStmt.TokenLiteral not 'return', got %q",
+				returnStmt.TokenLiteral())
+		}
+
+		if testLiteralExpression(t, returnStmt.ReturnValue, tt.expectedValue) {
+			return
+		}
+	}
+}
 
 func TestFunctionParameterParsing(t *testing.T) {
 	tests := []struct {
