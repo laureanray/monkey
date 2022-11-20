cc1ac5eaf86899870d790a169c0d6ac957665586
laureanray <laureanraybahala@gmail.com>
2022-11-20 22:30:43 +0800

    add: null and boolean

--- object/object.go
+++ object/object.go
@@ -6,6 +6,8 @@ type ObjectType string
 
 const (
 	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
 )
 
 type Object interface {
@@ -17,5 +19,17 @@ type Integer struct {
 	Value int64
 }
 
-func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+type Boolean struct {
+	Value bool
+}
+
+type Null struct{}
+
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+
+func (i *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (i *Boolean) Inspect() string  { return fmt.Sprintf("%t", i.Value) }
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string  { return "null" }
