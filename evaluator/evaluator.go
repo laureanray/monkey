261344dc276667f56d0c95a4f0f6e82819be01e2
laureanray <laureanraybahala@gmail.com>
2023-01-06 11:51:02 +0800

    add if else expression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -31,12 +31,40 @@ func Eval(node ast.Node) object.Object {
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 
+  case *ast.BlockStatement:
+    return evalStatements(node.Statements)
+
+  case *ast.IfExpression:
+    return evalIfExpression(node)
+
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil 
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+  condition := Eval(ie.Condition)
+
+  if isTruthy(condition) {
+    return Eval(ie.Consequence)
+  } else if ie.Alternative != nil {
+    return Eval(ie.Alternative)
+  } else {
+    return NULL
+  }
 } 
+
+func isTruthy(obj object.Object) bool { 
+  switch obj {
+  case NULL: return false
+  case TRUE: return true
+  case FALSE: return false
+  default:
+    return true
+  }
+}
 
 func evalInfixExpression(operator string, left, right object.Object) object.Object {
 	switch {
