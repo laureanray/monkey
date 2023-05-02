d74003b86b54622fd92551cd97acddfa7da01f0c
laureanray <laureanraybahala@gmail.com>
2023-05-02 18:36:39 +0800

    add token update parser

--- parser/parser.go
+++ parser/parser.go
@@ -65,6 +65,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
 	p.registerPrefix(token.IF, p.parseIfExpression)
 	p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
+	p.registerPrefix(token.STRING, p.parseStringLiteral)
 	// Read two tokens, so curToken and peekToken are both set
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
@@ -437,4 +438,8 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 func (p *Parser) noPrefixParseFnError(t token.TokenType) {
 	msg := fmt.Sprintf("no prefix parse function for %s found", t)
 	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) parseStringLiteral() ast.Expression {
+	return &ast.StringLiteral{Token: p.curToken, Value: p.curToken.Literal}
 }
