3f46782ce725fd082e0655a1f79cb5d41709b97a
laureanray <laureanraybahala@gmail.com>
2023-05-22 13:51:39 +0800

    feat: add string concatenation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -326,3 +326,18 @@ func TestStringLiteral(t *testing.T) {
 		t.Errorf("String has wrong value. got=%q", str.Value)
 	}
 }
+
+
+func TestStringConcatenation(t *testing.T) {
+  input := `"Hello" + " " + "World"`
+  evaluated := testEval(input)
+  str, ok := evaluated.(*object.String)
+  
+  if !ok {
+    t.Fatalf("object is not a String. got=%T (%+v)", evaluated, evaluated)
+  }
+
+  if str.Value != "Hello World" {
+    t.Errorf("String has wrong value. got=%q", str.Value)
+  }
+}
