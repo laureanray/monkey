e8a45ef7e74cef255d0bedbf5d62f869ed2f0659
laureanray <laureanraybahala@gmail.com>
2023-05-25 22:26:25 +0800

    array index works

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -29,8 +29,7 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 
 	case *ast.PrefixExpression:
 		right := Eval(node.Right, env)
-		if isError(right) {
-			return right
+		if isError(right) { return right
 		}
 		return evalPrefixExpression(node.Operator, right)
 
@@ -99,9 +98,44 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 	case *ast.StringLiteral:
 		return &object.String{Value: node.Value}
 
+  case *ast.IndexExpression:
+    left := Eval(node.Left, env)
+
+    if isError(left) {
+      return left
+    }
+
+    index := Eval(node.Index, env)
+
+    if isError(index) {
+      return index
+    }
+
+    return evalIndexExpression(left, index)
 	}
 
 	return nil
+}
+
+func evalIndexExpression(left, index object.Object) object.Object {
+  switch {
+  case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+    return evalArrayIndexExpression(left, index)
+  default:
+    return newError("index operator not supported: %s", left.Type())
+  }
+}
+
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+  arrayObject := array.(*object.Array)
+  idx := index.(*object.Integer).Value
+  max := int64(len(arrayObject.Elements) - 1)
+
+  if idx < 0 || idx > max {
+    return NULL
+  }
+
+  return arrayObject.Elements[idx]
 }
 
 func evalBlockStatement(block *ast.BlockStatement, env *object.Environment) object.Object {
