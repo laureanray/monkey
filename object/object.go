8610d0118de307f88fb5b2a8243b148f78c2662a
laureanray <laureanraybahala@gmail.com>
2023-05-28 18:26:36 +0800

    implement hashkey

--- object/object.go
+++ object/object.go
@@ -3,6 +3,7 @@ package object
 import (
 	"bytes"
 	"fmt"
+	"hash/fnv"
 	"monkey/ast"
 	"strings"
 )
@@ -68,9 +69,23 @@ type Null struct{}
 
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) HashKey() HashKey {
+  return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
 
-func (i *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
-func (i *Boolean) Inspect() string  { return fmt.Sprintf("%t", i.Value) }
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+func (b *Boolean) HashKey() HashKey {
+  var value uint64
+
+  if b.Value {
+    value = 1
+  } else {
+    value = 0
+  }
+
+  return HashKey{Type: b.Type(), Value: value }
+}
 
 func (n *Null) Type() ObjectType { return NULL_OBJ }
 func (n *Null) Inspect() string  { return "null" }
@@ -100,9 +115,14 @@ func (f *Function) Inspect() string {
 func (s *String) Type() ObjectType {
 	return STRING_OBJ
 }
-
 func (s *String) Inspect() string {
 	return s.Value
+}
+func (s *String) HashKey() HashKey {
+  h := fnv.New64a()
+  h.Write([]byte(s.Value))
+
+  return HashKey{Type: s.Type(), Value: h.Sum64()}
 }
 
 type Array struct {
@@ -125,3 +145,9 @@ func (ao *Array) Inspect() string {
 
   return out.String()
 }
+
+type HashKey struct {
+  Type ObjectType
+  Value uint64
+}
+
