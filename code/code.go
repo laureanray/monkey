1576a12ff88321119bea448502689ce9283ac741
laureanray <laureanraybahala@gmail.com>
2023-08-13 21:53:43 +0800

    add op lookup

--- code/code.go
+++ code/code.go
@@ -1,9 +1,30 @@
 package code
 
+import "fmt"
+
 type Instructions []byte
 
 type Opcode byte
 
 const (
-  OpConstant Opcode = iota
+	OpConstant Opcode = iota
 )
+
+type Definition struct {
+	Name         string
+	OperandWidth []int
+}
+
+var definitions = map[Opcode]*Definition{
+  OpConstant: {"OpConstant", []int{2}},
+}
+
+func Lookup(op byte) (*Definition, error) {
+  def, ok := definitions[Opcode(op)]
+
+  if !ok {
+    return nil, fmt.Errorf("opcode %d undefined", op)
+  }
+
+  return def, nil
+}
