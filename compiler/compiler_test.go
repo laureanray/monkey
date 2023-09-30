2df5da53f19e00ae20a0b5c4f4cd9bb08d2e5e7a
laureanray <laureanraybahala@gmail.com>
2023-09-30 22:08:21 +0800

    wip

---
+++ compiler/compiler_test.go
@@ -0,0 +1,48 @@
+package compiler
+
+import (
+	"monkey/code"
+	"testing"
+)
+
+type compilerTestCase struct {
+	input                string
+	expectedConstants    []interface{}
+	expectedInstructions []code.Instructions
+}
+
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []compilerTestCase{
+		{
+			input:             "1+2",
+			expectedConstants: []interface{}{1, 2},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpConstant, 1),
+			},
+		},
+	}
+
+}
+
+func runCompilerTests(t *testing.T, tests []compilerTestCase) {
+	t.Helper()
+
+	for _, tt := range tests {
+		program := parse(tt.input)
+		compiler := New()
+		err := compiler.Compile(program)
+
+		if err != nil {
+			t.Fatalf("compiler error: %s", err)
+		}
+
+		bytecode := compiler.Bytecode()
+
+		err = testInstructions(tt.expectedInstructions, bytecode.Instructions)
+
+		if err != nil {
+			t.Fatalf("testInstructions failed: %s", err)
+		}
+	}
+}
