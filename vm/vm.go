c1b8a4019318defe0ecf58d273ee99b6db6d6544
laureanray <laureanraybahala@gmail.com>
2023-10-01 21:32:55 +0800

    add vm struct

--- vm/vm.go
+++ vm/vm.go
@@ -1,3 +1,17 @@
 package vm
 
+import (
+	"monkey/code"
+	"monkey/object"
+)
+
 const StackSize = 2048
+
+type VM struct {
+	constants    []object.Object
+	instructions code.Instructions
+
+	stack []object.Object
+
+	sp int // Always point to the next value. Top of the stack is stack [sp-1]
+}
