f62fb6576100a892886e56c0d99d7e4ed531ae5f
laureanray <laureanraybahala@gmail.com>
2023-10-07 14:51:34 +0800

    implement pop and opadd on vm

--- vm/vm.go
+++ vm/vm.go
@@ -39,11 +39,17 @@ func (vm *VM) push(o object.Object) error {
 	if vm.sp >= StackSize {
 		return fmt.Errorf("stack overflow")
 	}
-	
+
 	vm.stack[vm.sp] = o
 	vm.sp++
 
 	return nil
+}
+
+func (vm *VM) pop() object.Object {
+	o := vm.stack[vm.sp-1]
+	vm.sp--
+	return o
 }
 
 // Heartbeat: the main loop, fetch decode execute cycle
@@ -59,6 +65,14 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+		case code.OpAdd:
+			right := vm.pop()
+			left := vm.pop()
+			leftValue := left.(*object.Integer).Value
+			rightValue := right.(*object.Integer).Value
+
+			result := leftValue + rightValue
+			vm.push(&object.Integer{Value: result})
 		}
 	}
 
